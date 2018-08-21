62658d8f31769f05bfde4506c17f13e6f0c2dc7f
wjjzs <15035820946@163.com>
2018-08-21 17:21:43 +0800

    wjj

--- src/main/java/com/dyhc/hospitalmanager/dao/CombinationAndCheckMapper.java
+++ src/main/java/com/dyhc/hospitalmanager/dao/CombinationAndCheckMapper.java
@@ -1,4 +1,15 @@
 package com.dyhc.hospitalmanager.dao;
 
+import com.dyhc.hospitalmanager.pojo.Combination;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface CombinationAndCheckMapper {
+    /**王娇
+     * 根据体检编号查询人员在该科室下的体检项，组合项
+     * @param checkId
+     * @return
+     */
+    Combination getCheckAndComByCheckId(@Param("checkId")Integer checkId);
 }
