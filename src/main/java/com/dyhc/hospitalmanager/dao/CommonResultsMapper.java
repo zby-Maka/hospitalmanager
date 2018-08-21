90b050714f434854d70301d24df834fe072c9431
crf-001 <927885007@qq.com>
2018-08-21 17:35:56 +0800

    曹锐锋

--- src/main/java/com/dyhc/hospitalmanager/dao/CommonResultsMapper.java
+++ src/main/java/com/dyhc/hospitalmanager/dao/CommonResultsMapper.java
@@ -6,10 +6,18 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 public interface CommonResultsMapper {
+
     /**王娇娇
      * 根据体检项id查常见结果表，默认显示第一条
      * @param checkId
      * @return
      */
     List<CommonResults> getCommResultsByCheckId(@Param("checkId")Integer checkId)throws Exception;
+
+    /**
+     * 根据体检项Id查询常见结果表（默认第一条）
+     * @param checkId
+     * @return
+     */
+    CommonResults getCommonResultsByCheckId(@Param("checkId")Integer checkId)throws Exception;
 }
--- src/main/java/com/dyhc/hospitalmanager/dao/ProposedDescriptionMapper.java
+++ src/main/java/com/dyhc/hospitalmanager/dao/ProposedDescriptionMapper.java
@@ -6,10 +6,20 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 public interface ProposedDescriptionMapper {
+
     /**王娇娇
      * 根据结果id查建议描述表，默认显示第一条
      * @param resultId
      * @return
      */
     List<ProposedDescription> getProposedByResultId(@Param("resultId") Integer resultId)throws Exception;
+
+    /**
+     *根据结果Id查建议描述表（默认第一条）
+     * @param resultId
+     * @return
+     */
+    ProposedDescription getProposedDescriptionByresultId(@Param("resultId")String resultId)throws Exception;
+
+
 }
