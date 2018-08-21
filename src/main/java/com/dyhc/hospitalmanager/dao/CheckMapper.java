95477b3be4cb37e223b2be86f335f6e80528bfc5
wjjzs <15035820946@163.com>
2018-08-21 17:40:19 +0800

    wjj

--- src/main/java/com/dyhc/hospitalmanager/dao/CheckMapper.java
+++ src/main/java/com/dyhc/hospitalmanager/dao/CheckMapper.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface CheckMapper {
     /**wjj
-     * 根据体检项id查出体检项数据，与接收的数据对比
+     * 科室是检验:根据体检项id查出体检项数据，与接收的数据对比
      * @param checkId
      * @return
      */
