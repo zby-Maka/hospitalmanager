cebfb92bdf770262f036ee5b1e64ec92efbdecf7
wjjzs <15035820946@163.com>
2018-08-21 17:45:13 +0800

    wjj

--- src/main/java/com/dyhc/hospitalmanager/dao/CommonResultsMapper.java
+++ src/main/java/com/dyhc/hospitalmanager/dao/CommonResultsMapper.java
@@ -8,16 +8,10 @@ import java.util.List;
 public interface CommonResultsMapper {
 
     /**王娇娇
-     * 根据体检项id查常见结果表，默认显示第一条
+     * 科室是检查:根据体检项id查常见结果表，默认显示第一条
      * @param checkId
      * @return
      */
     List<CommonResults> getCommResultsByCheckId(@Param("checkId")Integer checkId)throws Exception;
 
-    /**
-     * 根据体检项Id查询常见结果表（默认第一条）
-     * @param checkId
-     * @return
-     */
-    CommonResults getCommonResultsByCheckId(@Param("checkId")Integer checkId)throws Exception;
 }
