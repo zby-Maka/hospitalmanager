bfb7cf9d12813c6928ee70cd91fdcde061e1d6b4
17696011037 <40793606+17696011037@users.noreply.github.com>
2018-08-21 20:04:56 +0800

    zhou

--- src/main/java/com/dyhc/hospitalmanager/dao/PhysicalExaminationAndCombinationMapper.java
+++ src/main/java/com/dyhc/hospitalmanager/dao/PhysicalExaminationAndCombinationMapper.java
@@ -1,4 +1,15 @@
 package com.dyhc.hospitalmanager.dao;
 
+import org.apache.ibatis.annotations.Param;
+
 public interface PhysicalExaminationAndCombinationMapper {
+
+    /**
+     * 新增用户选择的组合项
+     * zby
+     * @param physicalExaminationId 组合项Id
+     * @param combinationId 体检表Id
+     * @return
+     */
+    Integer addPhysicalExaminationAndCombination(@Param("physicalExaminationId") String physicalExaminationId, @Param("combinationId") Integer combinationId);
 }
--- src/main/java/com/dyhc/hospitalmanager/dao/PhysicalExaminationMapper.java
+++ src/main/java/com/dyhc/hospitalmanager/dao/PhysicalExaminationMapper.java
@@ -1,6 +1,9 @@
 package com.dyhc.hospitalmanager.dao;
 
 import com.dyhc.hospitalmanager.pojo.PhysicalExamination;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface PhysicalExaminationMapper {
 
@@ -12,4 +15,12 @@ public interface PhysicalExaminationMapper {
      * @return
      */
     int addPhysicalExaminationInfo(PhysicalExamination physicalExamination)throws  Exception;
+
+    /**
+     * 根据用户Id查询历史体检记录
+     * zby
+     * @param personId 人员编号
+     * @return
+     */
+    List<PhysicalExamination> getPersonPhysicalExaminationList(@Param("personId") Integer personId);
 }
