cb7a3b015eb9e502ef2fd12282606fa21ed73460
crf-001 <927885007@qq.com>
2018-08-21 20:13:56 +0800

    曹锐峰

--- src/main/java/com/dyhc/hospitalmanager/dao/CostDetailMapper.java
+++ src/main/java/com/dyhc/hospitalmanager/dao/CostDetailMapper.java
@@ -1,4 +1,15 @@
 package com.dyhc.hospitalmanager.dao;
 
+import com.dyhc.hospitalmanager.pojo.CostDetail;
+import org.apache.ibatis.annotations.Param;
+
 public interface CostDetailMapper {
+
+    /**
+     * 添加费用明细表
+     * @param costDetail
+     * @return
+     */
+    Integer addCostDetail(@Param("costDetail")CostDetail costDetail)throws Exception;
+
 }
--- src/main/java/com/dyhc/hospitalmanager/dao/CostMapper.java
+++ src/main/java/com/dyhc/hospitalmanager/dao/CostMapper.java
@@ -1,4 +1,15 @@
 package com.dyhc.hospitalmanager.dao;
 
+import com.dyhc.hospitalmanager.pojo.Cost;
+import org.apache.ibatis.annotations.Param;
+
 public interface CostMapper {
+
+    /**
+     * 添加费用表
+     * @param cost
+     * @return
+     */
+    Integer addCost(@Param("cost")Cost cost)throws Exception;
+
 }
