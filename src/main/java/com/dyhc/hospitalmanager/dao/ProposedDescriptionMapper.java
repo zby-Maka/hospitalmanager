07b232cbda43a0313b861dd2b31e4eaebaa62290
wjjzs <15035820946@163.com>
2018-08-21 17:01:42 +0800

    wjj

--- src/main/java/com/dyhc/hospitalmanager/dao/ProposedDescriptionMapper.java
+++ src/main/java/com/dyhc/hospitalmanager/dao/ProposedDescriptionMapper.java
@@ -1,4 +1,15 @@
 package com.dyhc.hospitalmanager.dao;
 
+import com.dyhc.hospitalmanager.pojo.ProposedDescription;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface ProposedDescriptionMapper {
+    /**王娇娇
+     * 根据结果id查建议描述表，默认显示第一条
+     * @param resultId
+     * @return
+     */
+    List<ProposedDescription> getProposedByResultId(@Param("resultId") Integer resultId)throws Exception;
 }
