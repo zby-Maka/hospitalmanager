88ef2d11cd21c8a685ae8616f99d45c9bae7d590
ycy-1 <1176310511@qq.com>
2018-08-21 17:10:27 +0800

    ycy档案模块

--- src/main/java/com/dyhc/hospitalmanager/dao/ArchivesMapper.java
+++ src/main/java/com/dyhc/hospitalmanager/dao/ArchivesMapper.java
@@ -1,4 +1,30 @@
 package com.dyhc.hospitalmanager.dao;
 
+import com.dyhc.hospitalmanager.pojo.Archives;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * 档案接口
+ * ycy
+ */
 public interface ArchivesMapper {
+
+    /**
+     * 根据用户id查询档案信息
+     * author ycy
+     * @return
+     */
+    List<Archives>getArchivesByPerssonId(@Param("personId")Integer personId) throws  Exception;
+
+    /**
+     * 查询档案最后一位倒序
+     * @return
+     * author ycy
+     * @throws Exception
+     */
+    Archives getArchivesIdByDesc() throws Exception;
+
+
 }
