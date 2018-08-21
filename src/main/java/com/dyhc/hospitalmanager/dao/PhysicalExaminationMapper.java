2614e3234cbaa10822795e75662e451c7f4cc4ed
ycy-1 <1176310511@qq.com>
2018-08-21 19:28:03 +0800

    ycy档案模块

--- src/main/java/com/dyhc/hospitalmanager/dao/PhysicalExaminationMapper.java
+++ src/main/java/com/dyhc/hospitalmanager/dao/PhysicalExaminationMapper.java
@@ -1,6 +1,9 @@
 package com.dyhc.hospitalmanager.dao;
 
 import com.dyhc.hospitalmanager.pojo.PhysicalExamination;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface PhysicalExaminationMapper {
 
@@ -12,4 +15,12 @@ public interface PhysicalExaminationMapper {
      * @return
      */
     int addPhysicalExaminationInfo(PhysicalExamination physicalExamination)throws  Exception;
+
+    /**
+     * 根据档案编号查询体检信息
+     * author ycy
+     * @param archivesId
+     * @return
+     */
+    List<PhysicalExamination>getPhysicalExaminationInfoByArchivesId(@Param("archivesId") String archivesId);
 }
