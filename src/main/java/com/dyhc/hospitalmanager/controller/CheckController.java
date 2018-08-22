8c11a605028c19d9b510daedc6433c470d5e3a28
17696011037 <40793606+17696011037@users.noreply.github.com>
2018-08-22 18:33:33 +0800

    zhou

--- src/main/java/com/dyhc/hospitalmanager/controller/CheckController.java
+++ src/main/java/com/dyhc/hospitalmanager/controller/CheckController.java
@@ -1,12 +1,21 @@
 package com.dyhc.hospitalmanager.controller;
 
+import com.dyhc.hospitalmanager.pojo.Check;
 import com.dyhc.hospitalmanager.service.CheckService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CheckController{
 
     @Autowired
     private CheckService checkService;
+
+    @GetMapping("/getAllCheckList")
+    public List<Check> getAllCheckList() {
+        return checkService.getAllCheckList();
+    }
 }
