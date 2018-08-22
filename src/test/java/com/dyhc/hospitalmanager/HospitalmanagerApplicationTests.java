483b0006683a5fdabb10dfe9fe0cf5a4baae0565
crf-001 <927885007@qq.com>
2018-08-22 16:25:39 +0800

    Merge branch 'master' of E:\idea实例\hospitalmanager with conflicts.

--- src/test/java/com/dyhc/hospitalmanager/HospitalmanagerApplicationTests.java
+++ src/test/java/com/dyhc/hospitalmanager/HospitalmanagerApplicationTests.java
@@ -1,12 +1,9 @@
 package com.dyhc.hospitalmanager;
 
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -14,8 +11,6 @@ public class HospitalmanagerApplicationTests {
 
     @Test
     public void contextLoads() {
-
     }
-
 
 }
