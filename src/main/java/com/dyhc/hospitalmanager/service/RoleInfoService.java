876c0fdf61ba45d11438b0940d2691defe870102
aixiaobai <2504843010@qq.com>
2018-08-23 18:31:38 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	log/debug.log
    #	log/debug/debug.2018-08-21.log.gz
    #	log/info.log
    #	log/info/info.2018-08-21.log.gz

--- src/main/java/com/dyhc/hospitalmanager/service/RoleInfoService.java
+++ src/main/java/com/dyhc/hospitalmanager/service/RoleInfoService.java
@@ -1,4 +1,15 @@
 package com.dyhc.hospitalmanager.service;
 
+import com.dyhc.hospitalmanager.pojo.RoleInfo;
+import org.apache.ibatis.annotations.Param;
+
 public interface RoleInfoService {
+    /**
+     * 用户登录
+     * @param userName
+     * @param password
+     * @return
+     * @throws Exception
+     */
+    RoleInfo getRoleInfoLogin( String userName, String password) throws Exception;
 }
--- src/main/java/com/dyhc/hospitalmanager/service/impl/RoleInfoServiceImpl.java
+++ src/main/java/com/dyhc/hospitalmanager/service/impl/RoleInfoServiceImpl.java
@@ -1,6 +1,7 @@
 package com.dyhc.hospitalmanager.service.impl;
 
 import com.dyhc.hospitalmanager.dao.RoleInfoMapper;
+import com.dyhc.hospitalmanager.pojo.RoleInfo;
 import com.dyhc.hospitalmanager.service.RoleInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,4 +11,9 @@ public class RoleInfoServiceImpl implements RoleInfoService {
 
     @Autowired
     private RoleInfoMapper roleInfoMapper;
+
+    @Override
+    public RoleInfo getRoleInfoLogin(String userName, String password) throws Exception {
+        return roleInfoMapper.getRoleInfoLogin(userName, password);
+    }
 }
