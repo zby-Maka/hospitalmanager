9bbc6da4f0ec59fcd0aa38e8f4e0a9cc9eec9628
aixiaobai <2504843010@qq.com>
2018-08-23 18:01:48 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	log/debug.log
    #	log/debug/debug.2018-08-21.log.gz
    #	log/info.log
    #	log/info/info.2018-08-21.log.gz

--- src/main/java/com/dyhc/hospitalmanager/dao/RoleInfoMapper.java
+++ src/main/java/com/dyhc/hospitalmanager/dao/RoleInfoMapper.java
@@ -1,9 +1,20 @@
 package com.dyhc.hospitalmanager.dao;
 
+import com.dyhc.hospitalmanager.pojo.RoleInfo;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Mapper
 @Repository
 public interface RoleInfoMapper {
+    /**
+     * 用户登录
+     * @param userName
+     * @param password
+     * @return
+     * @throws Exception
+     */
+    RoleInfo getRoleInfoLogin(@Param("userName")String userName,@Param("password")String password) throws Exception;
+
 }
