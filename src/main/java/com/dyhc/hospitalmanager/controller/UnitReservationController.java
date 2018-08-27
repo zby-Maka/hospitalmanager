24e1d7002916048d2556113882f71ccec5ccc7c6
lwr <1195557074@qq.com>
2018-08-27 20:21:42 +0800

    lwr - 单位预约 别给我删掉哦 打死你们

--- src/main/java/com/dyhc/hospitalmanager/controller/UnitReservationController.java
+++ src/main/java/com/dyhc/hospitalmanager/controller/UnitReservationController.java
@@ -3,11 +3,17 @@ package com.dyhc.hospitalmanager.controller;
 import com.alibaba.fastjson.JSON;
 import com.dyhc.hospitalmanager.pojo.CompanyInfo;
 import com.dyhc.hospitalmanager.service.UnitReservationService;
+import com.dyhc.hospitalmanager.service.impl.ExcelServiceImpl;
+import com.dyhc.hospitalmanager.util.ResponseUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * 单位预约
@@ -16,6 +22,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class UnitReservationController {
     @Autowired
     private UnitReservationService unitReservationService;
+    @Autowired
+    private ExcelServiceImpl excelService;
 
     /**
      * 根据公司名称查询是否有公司信息
@@ -27,6 +35,49 @@ public class UnitReservationController {
     public String showCompanyInfo(@RequestParam("companyName") String companyName){
         CompanyInfo companyInfo = unitReservationService.showCompanyInfo(companyName);
         return JSON.toJSONString(companyInfo);
+    }
+
+    /**
+     * 用户下载Excel模板
+     * @return
+     * @throws IOException
+     */
+    @RequestMapping("/admin/model")
+    public ResponseEntity<byte[]> download2() throws IOException {
+        File file = excelService.buildXlsById();
+        return ResponseUtils.buildResponseEntity(file);
+    }
+
+    /**
+     * 公司对信息进行修改
+     * @param companyInfo
+     * @return
+     */
+    @RequestMapping("/updCompanyInfo.do")
+    @ResponseBody
+    public String updCompanyInfo(CompanyInfo companyInfo){
+        Integer content = 0;
+        Integer result = unitReservationService.updCompanyInfo(companyInfo);
+        if(result > 0){
+            content = 1;
+        }
+        return JSON.toJSONString(content);
+    }
+
+    /**
+     * 添加信息
+     * @param companyInfo
+     * @return
+     */
+    @RequestMapping("/addCompanyInfo.do")
+    @ResponseBody
+    public String addCompanyInfo(CompanyInfo companyInfo){
+        Integer content = 0;
+        Integer result = unitReservationService.addCompanyInfo(companyInfo);
+        if(result > 0){
+            content = 2;
+        }
+        return JSON.toJSONString(content);
     }
 
 }
--- src/main/resources/static/js/companybooking.js
+++ src/main/resources/static/js/companybooking.js
@@ -1,23 +1,44 @@
+//验证电话号
+$("input[name='telephone']").on("blur", function() {
+    var phone = $("input[name='telephone']").val();
+    if(phone != "") {
+        var checkResult = checkPhone(phone);
+        if(!checkResult) {
+            $("td[name='telephone']").text("请输入正确的电话号");
+        } else {
+            $("td[name='telephone']").text("");
+        }
+    } else {
+        $("td[name='telephone']").text("");
+    }
+});
+function checkPhone(telephone) {
+    var reg = /^1[3|4|5|7|8][0-9]\d{4,8}$/;
+    if(!reg.exec(telephone)) {
+        return false;
+    }
+    return true;
+}
 /*单位预约*/
 /*点击公司名称的失焦事件*/
 $("input[name=companyName]").blur(function () {
     //获取公司名称进行查询
     var companyName = $("input[name=companyName]").val();
-    alert(companyName);
     $.ajax({
         url:"/showCompanyInfo.do",
         data:{companyName:companyName},
         type:"get",
         success:function(data) {
-            alert(data);
-            $("input[name=companyId]").val(data.companyId);
+            if(data != undefined){
+                $("input[name=companyId]").val(data.companyId);
+                $("input[name=companyPrincipal]").val(data.companyPrincipal);
+                $("input[name=telephone]").val(data.telephone);
+                $("input[name=address]").val(data.address);
+                $("input[name=bankNumber]").val(data.bankNumber);
+                $("input[name=companyNature]").val(data.companyNature);
+                $("input[name=authorizedStrength]").val(data.authorizedStrength);
+            }
 
-            $("input[name=companyPrincipal]").val(data.companyPrincipal);
-            $("input[name=telephone]").val(data.telephone);
-            $("input[name=address]").val(data.address);
-            $("input[name=bankNumber]").val(data.bankNumber);
-            $("input[name=companyNature]").val(data.companyNature);
-            $("input[name=authorizedStrength]").val(data.authorizedStrength);
 
         },
         dataType:"json"
@@ -25,16 +46,55 @@ $("input[name=companyName]").blur(function () {
 
     //确定事件
     $("input[name=confirm]").click(function () {
-        alert("a");
         var companyId = $("input[name=companyId]").val();
-        alert(companyId);
-        if(companyId >0){
+        if(companyId > 0){
             //执行修改操作
-            alert("修改");
+            var content = $("#companyInfo").serialize();
+            $.ajax({
+                url:"/updCompanyInfo.do",
+                data:content,
+                type:"get",
+                dataType:"json",
+                success:function (data) {
+                    if(data>0){
+                        alert("操作成功");
+                        $("input[name=companyId]").val("");
+                        $("input[name=companyPrincipal]").val("");
+                        $("input[name=telephone]").val("");
+                        $("input[name=address]").val("");
+                        $("input[name=bankNumber]").val("");
+                        $("input[name=companyNature]").val("");
+                        $("input[name=authorizedStrength]").val("");
+                    }
+                }
+            });
+        }else{
+            var contents = $("#companyInfo").serialize();
+            $.ajax({
+                url: "/addCompanyInfo.do",
+                data: contents,
+                type: "get",
+                dataType:"json",
+                success: function (data) {
+                    if (data > 0) {
+                        alert("操作成功");
+                        $("input[name=companyName]").val("");
+                        $("input[name=companyId]").val("");
+                        $("input[name=companyPrincipal]").val("");
+                        $("input[name=telephone]").val("");
+                        $("input[name=address]").val("");
+                        $("input[name=bankNumber]").val("");
+                        $("input[name=companyNature]").val("");
+                        $("input[name=authorizedStrength]").val("");
+                    }
+                }
+
+            });
+
+
         }
-        if(companyId < 0 ){
-            //执行添加操作
-            alert("添加")
+
+        if(companyId = undefined  ) {
         }
     });
 
