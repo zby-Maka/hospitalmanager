df99e9bd43d2050ee84ff4e7a2edb97bf9036e8b
BloodlineKing <1136599243@qq.com>
2018-08-28 11:37:26 +0800

    刘杰

--- src/main/java/com/dyhc/hospitalmanager/config/SchedulerTask.java
+++ src/main/java/com/dyhc/hospitalmanager/config/SchedulerTask.java
@@ -1,13 +1,13 @@
 package com.dyhc.hospitalmanager.config;
 
 import com.dyhc.hospitalmanager.dao.RedisDao;
+import com.dyhc.hospitalmanager.util.GetFetureDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
 @Configuration
@@ -19,24 +19,10 @@ public class SchedulerTask {
 
     public void start() throws InterruptedException {
         Date date=new Date();
-        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
         String result=format.format(date);
         redisDao.delete(result);
-        String newKey=getFetureDate(8);
+        String newKey= GetFetureDate.getFetureDate(8);
         redisDao.setKey(newKey,"50");
-    }
-
-    /**
-     * 获取七天后的日期
-     * @param past
-     * @return
-     */
-    public String getFetureDate(int past) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.DAY_OF_YEAR, calendar.get(Calendar.DAY_OF_YEAR) + past);
-        Date today = calendar.getTime();
-        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
-        String result = format.format(today);
-        return result;
     }
 }
