ac2b0e546c2f5ebf6535ec43b99bcfb33f43713d
dxygithub <389228699@qq.com>
2018-09-18 15:46:11 +0800

    new yml

--- src/main/java/com/dyhc/hospitalmanager/controller/RoleInfoController.java
+++ src/main/java/com/dyhc/hospitalmanager/controller/RoleInfoController.java
@@ -4,15 +4,23 @@ import com.alibaba.fastjson.JSON;
 import com.dyhc.hospitalmanager.pojo.RoleInfo;
 import com.dyhc.hospitalmanager.pojo.Section;
 import com.dyhc.hospitalmanager.service.RoleInfoService;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-@RestController
+@Controller
 public class RoleInfoController{
 
     @Autowired
@@ -28,26 +36,52 @@ public class RoleInfoController{
      * @throws Exception
      */
     @RequestMapping(value = "login.do")
+    @ResponseBody
     public String getLogin(String userName, String password, Integer roletypeid, HttpServletRequest request) throws  Exception {
         String json = "";
-        RoleInfo roleInfo = roleInfoService.getRoleInfoLogin(userName, password);
-        //System.out.println(roleInfo.getRoleInfoId());
-        if (roleInfo == null) {
-            return JSON.toJSONString("用户名和密码错误");
-        } else {
-            Section section = null;
-            if (roleInfo.getSectionId() != null)
-                section = roleInfoService.getSectionIdByRoleInfoId(roleInfo.getRoleInfoId());
+        //获取用户实例
+        Subject subject=SecurityUtils.getSubject();
+        //判断当前用户是否通过认证
+        if(!subject.isAuthenticated()){
+            //如果未认证，就将用户名和密码封装成UsernamePasswordToken对象
+            UsernamePasswordToken usernamePasswordToken=new UsernamePasswordToken(userName,password);
+            //将用户的token存放与sessionManager中，让shiro记住该用户
+            usernamePasswordToken.setRememberMe(true);
 
-            HttpSession session = request.getSession();
-            session.setAttribute("roleInfo", roleInfo);
-            if (section != null) {
-                session.setAttribute("sectionId", section.getSectionId());
-                json = "{\"roletypeid\":\"" + roleInfo.getRoleTypeId() + "\",\"sectiontypeid\":\"" + section.getSectionTypeId() + "\"}";
-            } else {
-                json = "{\"roletypeid\":\"" + roleInfo.getRoleTypeId() + "\"}";
+            try {
+                //执行登录
+                subject.login(usernamePasswordToken);
+                json="{\"userName\":\""+userName+"\",\"state\":\"ok\"}";
+
+                //账号错误exception
+            }catch (UnknownAccountException e){
+                json="{\"state\":\"账号不存在！\"}";
+
+                //密码错误exception
+            } catch (IncorrectCredentialsException e){
+                json="{\"state\":\"密码错误！\"}";
+
+                //用户不存在exception
+            }catch (AuthenticationException e) {
+                json="{\"state\":\"账号异常！\"}";
+
+            }catch (Exception e){
+                e.printStackTrace();
             }
+        }else {
+            json="{\"state\":\"您已登录，无需重复登录！\"}";
         }
         return json;
+    }
+
+    /**
+     * 退出登录
+     * @return
+     */
+    @RequestMapping("/logout")
+    public String logout(){
+        Subject subject=SecurityUtils.getSubject();
+        subject.logout();
+        return "login";
     }
 }
