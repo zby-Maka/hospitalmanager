5319f983e0c46cd90d167c7c6d6870757679c48d
wangweimin961228 <13546247909@163.com>
2018-09-19 12:24:37 +0800

    wwm

--- src/main/java/com/dyhc/hospitalmanager/service/CombinationService.java
+++ src/main/java/com/dyhc/hospitalmanager/service/CombinationService.java
@@ -1,6 +1,7 @@
 package com.dyhc.hospitalmanager.service;
 
 import com.dyhc.hospitalmanager.pojo.Combination;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -11,4 +12,13 @@ public interface CombinationService {
      * @return
      */
     List<Combination> getAllCombinationList();
+
+    /**
+     * 根据一组组合项Id，查询组合项信息以及下的体检项信息
+     * @param combinationId 一组Array
+     * @return
+     * @throws Exception
+     */
+   List<Combination> getCheckByComArrayId(@Param("combinationId") Integer[] combinationId) throws Exception;
+
 }
